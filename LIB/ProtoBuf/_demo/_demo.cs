c86837043dcb5b8dd86866ed65e21cbb3dd00c0b
git-thinh <33323583+git-thinh@users.noreply.github.com>
2018-01-05 14:00:31 +0700

    A

---
+++ LIB/ProtoBuf/_demo/_demo.cs
@@ -0,0 +1,221 @@
+﻿using Enyim.Caching;
+using Enyim.Caching.Configuration;
+using Enyim.Caching.Memcached;
+using ProtoBuf;
+using ProtoBuf.Meta;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace core
+{
+    public class demo_protobuf
+    {
+        
+        public static void run6()
+        {
+
+            DMO_First first = new DMO_First { Foo = 12 };
+            DMO_Second second = Serializer.ChangeType<DMO_First, DMO_Second>(first);
+
+
+            ImplicitFieldPOCO foo = new ImplicitFieldPOCO
+            {
+                D_public = 100,
+                E_private = 101,
+                F_ignoreDirect = 102,
+                G_ignoreIndirect = 103,
+                H_nonSerialized = 104,
+                X_explicitField = 105,
+                Z_explicitProperty = 106
+            }; 
+
+            ImplicitFieldPOCOEquiv equiv = Serializer.ChangeType<ImplicitFieldPOCO, ImplicitFieldPOCOEquiv>(foo);
+        }
+
+
+        public void run5()
+        {
+            Tst t = new Tst();
+            t.ValInt = 128;
+            t.Str1 = "SOme string text value ttt";
+            t.ArrayData = new byte[] { };
+
+            MemoryStream stm = new MemoryStream();
+            Serializer.Serialize(stm, t);
+            Console.WriteLine(stm.Length);
+        }
+
+        public static void run4()
+        {
+            Database db0 = LoadDatabaseFromFile<Database>(RuntimeTypeModel.Default);
+
+            var model = TypeModel.Create();
+
+            var db1 = LoadDatabaseFromFile<Database>(model);
+
+            model.CompileInPlace();
+            var db2 = LoadDatabaseFromFile<Database>(model);
+
+            var db3 = LoadDatabaseFromFile<Database>(model.Compile());
+
+            //var db4 = LoadDatabaseFromFile<Database>(model.Compile("NWindModel", "NWindModel.dll"));
+
+            string proto = Serializer.GetProto<Database>();
+
+        }
+
+        public static void run3()
+        {
+            byte[] blob = File.ReadAllBytes("nwind.proto.bin");
+            using (MemoryStream ms = new MemoryStream(blob))
+            {
+                var model = ProtoBuf.Meta.TypeModel.Create();
+                Type type = typeof(Database);
+
+                model.Deserialize(ms, null, type);
+
+                var compiled = model.Compile();
+
+                /*erializer.PrepareSerializer<Database>();
+                Serializer.Deserialize<Database>(ms);*/
+                Stopwatch watch = Stopwatch.StartNew();
+                for (int i = 0; i < 1; i++)
+                {
+                    ms.Position = 0;
+
+                    var o1 = compiled.Deserialize(ms, null, type);
+
+
+                    var o2 = Serializer.Deserialize<Database>(ms);
+
+                }
+                watch.Stop();
+
+                Console.WriteLine(watch.ElapsedMilliseconds);
+                //if (Debugger.IsAttached)
+                //{
+                //    Console.WriteLine("(press any key)");
+                //    Console.ReadKey();
+                //}
+
+            }
+        }
+
+        public static void run2()
+        {
+
+            BasicType original = new BasicType { Id = 123, Name = "abc" }, clone;
+            using (var client = new MemcachedClient())
+            {
+                client.Store(StoreMode.Set, "ShouldBeAbleToCacheBasicTypes", original);
+            }
+
+            using (var client = new MemcachedClient())
+            {
+                clone = client.Get<BasicType>("ShouldBeAbleToCacheBasicTypes");
+            }
+        }
+
+
+        public static void run1()
+        {
+
+            MemcachedClientConfiguration config = new MemcachedClientConfiguration();
+            config.Servers.Add(new IPEndPoint(IPAddress.Loopback, 11211));
+            //config.Protocol = MemcachedProtocol.Binary;
+            //config.Authentication.Type = typeof(PlainTextAuthenticator);
+            //config.Authentication.Parameters["userName"] = "demo";
+            //config.Authentication.Parameters["password"] = "demo";
+
+            var mc = new MemcachedClient(config);
+            for (var i = 0; i < 100; i++)
+                mc.Store(StoreMode.Set, "Hello", "World");
+        }
+
+        public static void run0()
+        {
+            //var person = new mPerson
+            //{
+            //    Id = 12345,
+            //   // Name = "Fred",
+            //    Address = new mAddress
+            //    {
+            //        Line1 = "Flat 1",
+            //        Line2 = "The Meadows"
+            //    }
+            //};
+            //var person2 = new mPerson
+            //{
+            //    Id = 12345,
+            //    //Name = "Nguyễn Văn Thịnh",
+            //    Address = new mAddress
+            //    {
+            //        Line1 = "Flat 1",
+            //        Line2 = "The Meadows"
+            //    }
+            //};
+
+            //using (var file = File.Create("person.bin"))
+            //{
+            //    Serializer.Serialize<mPerson[]>(file,new mPerson[] { person, person2 });
+            //}
+
+            List<mPerson> newPerson;
+            using (var file = File.OpenRead("person.bin"))
+            {
+                newPerson = Serializer.Deserialize<List<mPerson>>(file);
+            }
+
+
+        }//
+
+
+        public static T LoadDatabaseFromFile<T>(TypeModel model) where T : class, new()
+        {
+            using (Stream fs = File.OpenRead("nwind.proto.bin"))
+            {
+                return (T)model.Deserialize(fs, null, typeof(T));
+            }
+        }
+
+        static Database ReadFromFile(string path)
+        {
+            Database database;
+            using (Stream fs = File.OpenRead(path))
+            {
+                database = Serializer.Deserialize<Database>(fs);
+                fs.Close();
+            }
+            return database;
+        }
+
+        static void WriteToFile(string path, Database database)
+        {
+            using (Stream fs = File.Create(path))
+            {
+                Serializer.Serialize(fs, database);
+                fs.Close();
+            }
+        }
+
+        public void ShouldBeAbleToCacheBasicTypes()
+        {
+            BasicType original = new BasicType { Id = 123, Name = "abc" }, clone;
+            using (var client = new MemcachedClient())
+            {
+                client.Store(StoreMode.Set, "ShouldBeAbleToCacheBasicTypes", original);
+            }
+            using (var client = new MemcachedClient())
+            {
+                clone = client.Get<BasicType>("ShouldBeAbleToCacheBasicTypes");
+            }
+        }
+
+    }//end class
+
+
+}
---
+++ LIB/ProtoBuf/_demo/_demo_model.cs
@@ -0,0 +1,234 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace core
+{
+
+    [ProtoContract]
+    [ProtoInclude(2, typeof(Bar))]
+    public class Foo
+    {
+        [ProtoMember(1)]
+        public string BaseProp { get; set; }
+    }
+
+    [ProtoContract]
+    public class Bar : Foo
+    {
+        [ProtoMember(1)]
+        public string ChildProp { get; set; }
+    }
+
+
+
+
+
+    [ProtoContract]
+    class DMO_First
+    {
+        [ProtoMember(5)]
+        public int Foo { get; set; }
+    }
+     
+    [ProtoContract(DataMemberOffset = 2)]
+    //[ProtoContract]
+    class DMO_Second
+    {
+        [ProtoMember(3)]
+        public int Bar { get; set; }
+    }
+
+
+    [ProtoContract]
+    public class mAddress
+    {
+        [ProtoMember(1)]
+        public string Line1 { get; set; }
+        [ProtoMember(2)]
+        public string Line2 { get; set; }
+    }
+
+    [ProtoContract]
+    public class mPerson
+    {
+        [ProtoMember(1)]
+        public int Id { get; set; }
+
+        [ProtoMember(2)]
+        public string Name { set; get; }
+
+        [ProtoMember(3)]
+        public mAddress Address { get; set; }
+
+        [ProtoMember(4)]
+        public string Email { get; set; }
+
+    }
+
+
+    [ProtoContract, Serializable]
+    public partial class Order
+    {
+        [ProtoMember(1)]
+        public int OrderID { set; get; }
+
+        [ProtoMember(2)]
+        public string CustomerID { set; get; }
+
+        [ProtoMember(3)]
+        public System.Nullable<int> EmployeeID { set; get; }
+
+        [ProtoMember(4, DataFormat = Database.SubObjectFormat)]
+        public System.Nullable<System.DateTime> OrderDate { set; get; }
+
+        [ProtoMember(5, DataFormat = Database.SubObjectFormat)]
+        public System.Nullable<System.DateTime> RequiredDate { set; get; }
+
+        [ProtoMember(6, DataFormat = Database.SubObjectFormat)]
+        public System.Nullable<System.DateTime> ShippedDate { set; get; }
+
+        [ProtoMember(7)]
+        public System.Nullable<int> ShipVia { set; get; }
+
+        [ProtoMember(8, DataFormat = Database.SubObjectFormat)]
+        public System.Nullable<decimal> Freight { set; get; }
+
+        [ProtoMember(9)]
+        public string ShipName { set; get; }
+
+        [ProtoMember(10)]
+        public string ShipAddress { set; get; }
+
+        [ProtoMember(11)]
+        public string ShipCity { set; get; }
+
+        [ProtoMember(12)]
+        public string ShipRegion { set; get; }
+
+        [ProtoMember(13)]
+        public string ShipPostalCode { set; get; }
+
+        [ProtoMember(14)]
+        public string ShipCountry { set; get; }
+    }
+
+    [ProtoContract, Serializable]
+    public class Database
+    {
+        public const DataFormat SubObjectFormat = DataFormat.Default;
+        [ProtoMember(1, DataFormat = Database.SubObjectFormat)]
+        public List<Order> Orders { get; private set; }
+
+        public Database()
+        {
+            Orders = new List<Order>();
+        }
+    }
+
+    [ProtoContract]
+    internal class Tst
+    {
+        [ProtoMember(1)]
+        public int ValInt
+        {
+            get;
+            set;
+        }
+
+        [ProtoMember(2)]
+        public byte[] ArrayData
+        {
+            get;
+            set;
+        }
+
+        [ProtoMember(3)]
+        public string Str1
+        {
+            get;
+            set;
+        }
+    }
+
+
+
+    [ProtoContract(ImplicitFields = ImplicitFields.AllFields, ImplicitFirstTag = 4)]
+    [ProtoPartialIgnore("g_ignoreIndirect")]
+    public class ImplicitFieldPOCO
+    {
+        public event EventHandler Foo;
+        protected virtual void OnFoo()
+        {
+            if (Foo != null) Foo(this, EventArgs.Empty);
+        }
+        public Action Bar;
+
+        public int D_public;
+
+        private int e_private;
+        public int E_private
+        {
+            get { return e_private; }
+            set { e_private = value; }
+        }
+
+        [ProtoIgnore]
+        private int f_ignoreDirect;
+        public int F_ignoreDirect
+        {
+            get { return f_ignoreDirect; }
+            set { f_ignoreDirect = value; }
+        }
+
+        private int g_ignoreIndirect;
+        public int G_ignoreIndirect
+        {
+            get { return g_ignoreIndirect; }
+            set { g_ignoreIndirect = value; }
+        }
+        [NonSerialized]
+        public int H_nonSerialized;
+
+        [ProtoMember(1)]
+        private int x_explicitField;
+
+        public int X_explicitField
+        {
+            get { return x_explicitField; }
+            set { x_explicitField = value; }
+        }
+
+        [ProtoIgnore]
+        private int z_explicitProperty;
+        [ProtoMember(2)]
+        public int Z_explicitProperty
+        {
+            get { return z_explicitProperty; }
+            set { z_explicitProperty = value; }
+        }
+
+    }
+
+    [ProtoContract]
+    public class ImplicitFieldPOCOEquiv
+    {
+        [ProtoMember(4)]
+        public int D { get; set; }
+        [ProtoMember(5)]
+        public int E { get; set; }
+        [ProtoMember(1)]
+        public int X { get; set; }
+        [ProtoMember(2)]
+        public int Z { get; set; }
+    }
+
+    [Serializable]
+    public class BasicType
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
