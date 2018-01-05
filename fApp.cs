f2857ac6272f12a9836fb6994d6155198cd72056
git-thinh <33323583+git-thinh@users.noreply.github.com>
2018-01-05 13:43:28 +0700

    Initial commit

---
+++ fApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace gcc_build_app
+{
+    public partial class fApp : Form
+    {
+        public fApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
