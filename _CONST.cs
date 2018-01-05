b15ff4d0b2d46cf3745397136d6e94a9fe8fc3cb
git-thinh <33323583+git-thinh@users.noreply.github.com>
2018-01-05 13:50:28 +0700

    a

---
+++ _CONST.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gcc_build_app
+{
+    public class _CONST
+    {
+        public const string APP_NAME = "GCC BUILD";
+        public const int APP_WIDTH = 800;
+        public const int APP_HEIGHT = 400;
+    }
+}
